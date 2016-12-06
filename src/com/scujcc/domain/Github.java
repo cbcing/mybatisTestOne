70a163542fcc55a22a6f13b81a16b01ed841a238
David <cbc903205927@qq.com>
2016-12-06 21:57:25 +0800

    github test

---
+++ src/com/scujcc/domain/Github.java
@@ -0,0 +1,16 @@
+package com.scujcc.domain;
+
+/**
+ * Created by David on 16/12/6.
+ */
+public class Github {
+    private String github;
+
+    public String getGithub() {
+        return github;
+    }
+
+    public void setGithub(String github) {
+        this.github = github;
+    }
+}
