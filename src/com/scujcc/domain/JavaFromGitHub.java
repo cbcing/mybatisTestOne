cf62a898957f528d1ecd11f1b3686bc438b76405
cbcing <903205927@qq.com>
2016-12-06 22:04:17 +0800

    Create JavaFromGitHub.java
    get file from GitHub in IDEA

---
+++ src/com/scujcc/domain/JavaFromGitHub.java
@@ -0,0 +1,13 @@
+package com.scujcc.domain;
+
+public class JavaFromGitHub{
+    private String test;
+    
+    public String getTest(){
+        return test;
+    }
+    
+    public void setTest(String test){
+        this.test = test;
+    }
+}
